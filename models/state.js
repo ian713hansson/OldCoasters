a92a02d442ba5cc8b5d5ad04c69b10f12dcec5ef
Ian Hansson <ian713hansson@gmail.com>
2022-07-21 11:00:32 -0500

    update schemas

---
+++ models/state.js
@@ -0,0 +1,10 @@
+const { Schema } = require('mongoose')
+
+const State = new Schema(
+    {
+        name: { type: String, required: true },
+    },
+    { timestamps: true }
+)
+
+module.exports = State
