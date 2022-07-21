6784a587e57177600ee78a0ebd96ab852b396541
Alex Campo <abrettc@gmail.com>
2022-07-21 13:27:06 -0500

    updated router/controller files for find coaster and add coaster

---
+++ controllers/index.js
@@ -0,0 +1,29 @@
+const { Coaster, State } = require('./models')
+
+const getCoasterById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const currentCoaster = await Coaster.findById(id)
+    if (!currentCoaster) {
+      return res.status(404).send('This coaster does not exist')
+    }
+    return res.status(201).json({ currentCoaster })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const addCoaster = async (req, res) => {
+  try {
+    const newCoaster = await new Coaster(req.body)
+    await plant.save()
+    return res.status(201).json({ newCoaster })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+module.exports = {
+  getCoasterById,
+  addCoaster
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const { Router } = require('express')
 const router = Router()
+const controllers = require('../controllers')
 
 router.get('/', (req, res) => res.send('This is the home page'))
+
+router.get('./coasters/:id', controllers.getCoasterById)
+
+router.post('./coasters/:id', controllers.addCoaster)
 
 module.exports = router
