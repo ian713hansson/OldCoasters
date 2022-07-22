a85d46b2ba78e93a2669be9b74d60e83a9549b75
Alex Campo <abrettc@gmail.com>
2022-07-22 08:23:52 -0500

    bugfixes on browserrouter in react

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,12 @@ function App() {
   return (
     <div>
       <h1> React homepage</h1>
+
       <Routes>
+        <Route path="/coasters" element={<CoasterCard />} />
         <Route path="/" element={<CoasterCard />} />
       </Routes>
     </div>
   )
 }
-
 export default App
