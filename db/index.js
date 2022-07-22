baf69cac58c10f713ea5e3b84a7f408b7f5d6a6c
Alex Campo <abrettc@gmail.com>
2022-07-22 12:12:49 -0500

    Deploying to Heroku

--- db/index.js
+++ db/index.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb+srv://alexcampo5:GASEI2022@cluster0.6yptjno.mongodb.net/OldCoasters'
 
 mongoose
-  .connect(
-    'mongodb+srv://alexcampo5:GASEI2022@cluster0.6yptjno.mongodb.net/OldCoasters'
-  )
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
